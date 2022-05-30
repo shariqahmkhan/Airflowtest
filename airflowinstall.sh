4137b017a4da90d3859544fae5bc19cd1acc1066
Shariq Ahmed Khan <shariqahmkhan@gmail.com>
2022-05-30 23:27:21 +0200

    Update airflowinstall.sh

--- airflowinstall.sh
+++ airflowinstall.sh
@@ -4,15 +4,15 @@ export AIRFLOW_HOME=~/airflow
 
 # Install Airflow using the constraints file
 AIRFLOW_VERSION=2.3.1
-PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.7
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
 # For example: https://raw.githubusercontent.com/apache/airflow/constraints-2.3.1/constraints-3.7.txt
-pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+pip3 install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}" --ignore-uninstall six
 
 # The Standalone command will initialise the database, make a user,
 # and start all components for you.
-airflow standalone
+airflow standalone &
 
 # Visit localhost:8080 in the browser and use the admin account details
 # shown on the terminal to login.
